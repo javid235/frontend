0468aa8e0350226c4b6456f6faac54f152bb456e
javid235 <mohamedjavid235@gmail.com>
2024-10-13 23:37:42 +0530

    #

--- src/App.js
+++ src/App.js
@@ -17,15 +17,15 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category='men' />} />
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category='women' />} />
-          <Route path='/kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
-          <Route path='product' element={<Product />}>
+          <Route path='/frontend' element={<Shop />} />
+          <Route path='/frontend/mens' element={<ShopCategory banner={men_banner} category='men' />} />
+          <Route path='/frontend/womens' element={<ShopCategory banner={women_banner} category='women' />} />
+          <Route path='/frontend/kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
+          <Route path='/frontend/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/login' element={<LoginSignup />} />
+          <Route path='/frontend/cart' element={<Cart />} />
+          <Route path='/frontend/login' element={<LoginSignup />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
